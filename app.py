080c743eb4040b9c1561a58b54e9c7d4a91caeac
Joseph Watters <jwatters@jawbone.com>
2017-01-11 16:44:13 -0800

    Replace argument parser with optparse and add comments to the scrape function

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import optparse
+
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,76 +10,124 @@ from selenium.webdriver.support import expected_conditions as EC
 from twilio.rest import TwilioRestClient
 from config import CONFIG
 
-def main(argv):
+def main():
     """
-    Missing docstring
+    Perform a search on southwest.com for the given flight parameters.
     """
+    options = parse_options()
+    scrape(options)
 
-    # Get the command line arguments.
-    args_dict = {}
-
-    # If no interval was specified, assume a default interval of 3 hours.
-    args_dict['--interval'] = '180'
-
-    for arg, item in enumerate(argv):
-        if argv[arg] == '--one-way':
-            args_dict[argv[arg]] = argv[arg]
-        if argv[arg] == '--from':
-            args_dict[argv[arg]] = argv[arg + 1]
-        elif argv[arg] == '--to':
-            args_dict[argv[arg]] = argv[arg + 1]
-        elif argv[arg] == '--departure-date':
-            args_dict[argv[arg]] = argv[arg + 1]
-        elif argv[arg] == '--return-date':
-            args_dict[argv[arg]] = argv[arg + 1]
-        elif argv[arg] == '--passengers':
-            args_dict[argv[arg]] = argv[arg + 1]
-        elif argv[arg] == '--desired-total':
-            args_dict[argv[arg]] = argv[arg + 1]
-        elif argv[arg] == '--interval':
-            args_dict[argv[arg]] = argv[arg + 1]
-
-    scrape(args_dict)
-
-
-def scrape(args_dict):
+def parse_options():
     """
-    Missing docstring
+    Parse the command line for search parameters.
+    """
+    usage = ("")
+    parser = optparse.OptionParser(usage=usage)
+
+    parser.add_option(
+        "--one-way",
+        action="store_true",
+        default=False,
+        help="If present, the search will be limited to one-way tickets.")
+
+    parser.add_option(
+        "--depart",
+        action="store",
+        type="string",
+        dest="depart",
+        help="Origin airport code.")
+
+    parser.add_option(
+        "--arrive",
+        action="store",
+        type="string",
+        dest="arrive",
+        help="Destination airport code.")
+
+    parser.add_option(
+        "--departure-date",
+        action="store",
+        type="string",
+        dest="departure_date",
+        help="Date of departure flight.")
+
+    parser.add_option(
+        "--return-date",
+        action="store",
+        type="string",
+        dest="return_date",
+        help="Date of return flight.")
+
+    parser.add_option(
+        "--passengers",
+        action="store",
+        type="string",
+        dest="passengers",
+        help="Number of passengers.")
+
+    parser.add_option(
+        "--desired-total",
+        action="store",
+        type="string",
+        dest="desired_total",
+        help="Ceiling on the total cost of flights.")
+
+    parser.add_option(
+        "--interval",
+        action="store",
+        type="string",
+        default=1,
+        dest="interval",
+        help="Refresh time period.")
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args[0]
+
+def scrape(options):
+    """
+    Run scraper on Southwest.com.
+    If we find a flight that meets our search parameters, send an SMS message.
     """
 
     while True:
         # PhantomJS is headless, so it doesn't open up a browser.
         browser = webdriver.PhantomJS()
-        browser.get('https://www.southwest.com/')
+        browser.get("https://www.southwest.com/")
 
-        # Assumed to be round trip by default.
-        one_way_bool = False
-        if '--one-way' in args_dict:
-            if args_dict['--one-way'] == '--one-way':
-                one_way_bool = True
-                one_way_elem = browser.find_element_by_id('trip-type-one-way')
-                one_way_elem.click()
+        if options.one_way:
+            # Set one way trip with click event
+            one_way_elem = browser.find_element_by_id("trip-type-one-way")
+            one_way_elem.click()
 
-        depart_airport = browser.find_element_by_id('air-city-departure')
-        depart_airport.send_keys(args_dict['--from'])
-        arrive_airport = browser.find_element_by_id('air-city-arrival')
-        arrive_airport.send_keys(args_dict['--to'])
-        depart_date = browser.find_element_by_id('air-date-departure')
+        # Set the departing airport
+        depart_airport = browser.find_element_by_id("air-city-departure")
+        depart_airport.send_keys(options.depart)
+
+        # Set the arrival airport
+        arrive_airport = browser.find_element_by_id("air-city-arrival")
+        arrive_airport.send_keys(options.arrive)
+
+        # Set departure date
+        depart_date = browser.find_element_by_id("air-date-departure")
         depart_date.clear()
-        depart_date.send_keys(args_dict['--departure-date'])
+        depart_date.send_keys(options.departure_date)
 
-        if not one_way_bool:
-            return_date = browser.find_element_by_id('air-date-return')
+        if not options.one_way:
+            # Set return date
+            return_date = browser.find_element_by_id("air-date-return")
             return_date.clear()
-            return_date.send_keys(args_dict['--return-date'])
+            return_date.send_keys(options.return_date)
 
-        passengers = browser.find_element_by_id('air-pax-count-adults')
+        passengers = browser.find_element_by_id("air-pax-count-adults")
         browser.execute_script("arguments[0].removeAttribute('readonly', 0);", passengers)
         passengers.click()
         passengers.clear()
-        passengers.send_keys(args_dict['--passengers'])
+        
+        # Set passenger count
+        passengers.send_keys(options.passengers)
         passengers.click()
-        search = browser.find_element_by_id('jb-booking-form-submit-button')
+        search = browser.find_element_by_id("jb-booking-form-submit-button")
         search.click()
 
         outbound_array = []
@@ -85,10 +135,10 @@ def scrape(args_dict):
 
         # Webdriver might be too fast. Tell it to slow down.
         wait = WebDriverWait(browser, 120)
-        wait.until(EC.element_to_be_clickable((By.ID, 'faresOutbound')))
+        wait.until(EC.element_to_be_clickable((By.ID, "faresOutbound")))
 
-        outbound_fares = browser.find_element_by_id('faresOutbound')
-        outbound_prices = outbound_fares.find_elements_by_class_name('product_price')
+        outbound_fares = browser.find_element_by_id("faresOutbound")
+        outbound_prices = outbound_fares.find_elements_by_class_name("product_price")
 
         for price in outbound_prices:
             realprice = price.text.replace("$", "")
@@ -96,9 +146,9 @@ def scrape(args_dict):
 
         lowest_outbound_fare = min(outbound_array)
 
-        if not one_way_bool:
-            return_fares = browser.find_element_by_id('faresReturn')
-            return_prices = return_fares.find_elements_by_class_name('product_price')
+        if not options.one_way:
+            return_fares = browser.find_element_by_id("faresReturn")
+            return_prices = return_fares.find_elements_by_class_name("product_price")
 
             for price in return_prices:
                 realprice = price.text.replace("$", "")
@@ -107,38 +157,38 @@ def scrape(args_dict):
             lowest_return_fare = min(return_array)
             real_total = lowest_outbound_fare + lowest_return_fare
 
-            print('[%s] Current Lowest Outbound Fare: $%s.' % (
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            print("[%s] Current Lowest Outbound Fare: $%s." % (
+                datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 str(lowest_outbound_fare)))
 
-            print('[%s] Current Lowest Return Fare: $%s.' % (
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            print("[%s] Current Lowest Return Fare: $%s." % (
+                datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 str(lowest_return_fare)))
 
         else:
             real_total = lowest_outbound_fare
-            print('[%s] Current Lowest Outbound Fare: $%s.' % (
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            print("[%s] Current Lowest Outbound Fare: $%s." % (
+                datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 str(lowest_outbound_fare)))
 
         # Found a good deal. Send a text via Twilio and then stop running.
-        if real_total < int(args_dict['--desired-total']):
-            print('[%s] Found a deal. Desired total: $%s. Current Total: $%s.' % (
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                args_dict['--desired-total'], str(real_total)))
+        if real_total < int(options.desired_total):
+            print("[%s] Found a deal. Desired total: $%s. Current Total: $%s." % (
+                datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                options.desired_total, str(real_total)))
 
-            client = TwilioRestClient(CONFIG.get('account_sid'), CONFIG.get('auth_token'))
+            client = TwilioRestClient(CONFIG.get("account_sid"), CONFIG.get("auth_token"))
 
             client.messages.create(
-                to=CONFIG.get('to_number'),
-                from_=CONFIG.get('from_number'),
-                body='[%s] Found a deal. Desired total: $%s. Current Total: $%s' % (
-                    datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                    args_dict['--desired-total'],
+                to=CONFIG.get("to_number"),
+                from_=CONFIG.get("from_number"),
+                body="[%s] Found a deal. Desired total: $%s. Current Total: $%s" % (
+                    datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    options.desired_total,
                     str(real_total)))
 
             print(
-                '[%s] Text message sent!' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                "[%s] Text message sent!" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             )
 
             sys.exit()
@@ -147,10 +197,11 @@ def scrape(args_dict):
             '''
             [%s] Couldn\'t find a deal under the amount you specified.
             Trying again to find cheaper prices...
-            ''' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            ''' % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         )
 
         # Keep scraping according to the interval the user specified.
-        time.sleep(int(args_dict['--interval']) * 60)
+        time.sleep(int(options.interval) * 60)
 
-main(sys.argv[1:])
+if __name__ == "__main__":
+    main()
